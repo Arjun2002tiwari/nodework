5dda167b2d7fdb208ccf09983db0f00c6a24ac95
Arjun2002tiwari <Arjarjun200@gmail.com>
2022-11-19 23:53:00 +0530

    changes

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/',(req,res)=>{
 app.use('/api/upload-profile',require('./Routes/user-profile'));
 app.use('/api/create',require('./Routes/user-create'));
 app.use('/api/upload-collections',require('./Routes/user-collections'));
-app.use('/api/keywords',require('./Routes/Keyword'))
+app.use('/api/keywords',require('./Routes/Keyword'));
 app.use('/api/profile',express.static('./upload/images'));
 
 app.use('/api/news',express.static('./NewsImages/images'));
