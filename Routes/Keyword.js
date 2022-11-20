33f41df71f0ceadba8842c2ce13c0775c838d101
Arjun2002tiwari <Arjarjun200@gmail.com>
2022-11-20 10:53:51 +0530

    changes

--- Routes/Keyword.js
+++ Routes/Keyword.js
@@ -3,17 +3,23 @@ const router=express.Router();
 const db=require('../database/db');
 const key=require('../Routes/News-upload');
 
-//var id=;
-router.post("/",(req,res)=>{
-    let data={username:req.body.username,id:req.body.id};
-    console.log(data.username);
-    //data={username:"arjun754",id:1};
 
-    let sql="INSERT INTO COLLECTIONS SET ?";
+router.post("/",(req,res)=>{
+    let data={
+        id:req.body.id,
+        k1:req.body.k1,
+        k2:req.body.k2,
+        k3:req.body.k3,
+        k4:req.body.k4,
+        k5:req.body.k5
+    };
+    
+
+    let sql="INSERT INTO KEYWORD SET ?";
    
     con.query(sql,data,(err,result)=>{
         if(err) throw err;
-        res.send(JSON.stringify({status:200,error:null,response:"Collections added!"}));
+        res.send(JSON.stringify({status:200,error:null,response:"Keywords added!"}));
     }); 
 });
  
