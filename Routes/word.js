c6d8691522830e30e217089792d91c03b019b0e6
Arjun2002tiwari <Arjarjun200@gmail.com>
2022-11-20 16:45:31 +0530

    comit

--- Routes/word.js
+++ Routes/word.js
@@ -4,10 +4,18 @@ const db=require('../database/db');
 
 var con=db.con;
 router.post("/",(req,res)=>{
-    let data={name:req.body.name};
+    let data={
+        id:req.body.id,
+        k1:req.body.k1,
+        k2:req.body.k2,
+        k3:req.body.k3,
+        k4:req.body.k4,
+        k5:req.body.k5,
+
+    };
     console.log(data);
     
-    let sql="INSERT INTO CHECK SET ?";
+    let sql="INSERT INTO KEYWORD SET ?";
    
     con.query(sql,data,(err,result)=>{
         if(err) throw err;
